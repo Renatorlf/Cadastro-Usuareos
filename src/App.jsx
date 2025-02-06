4b8fcffac4aedd73ed491a453f4b634b2f9682df
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2025-02-05 21:14:10 -0300

    Aula React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+//É aqui onde se escreve todos os códigos do componente REACT
+function App() {
+
+  return (
+      <div>
+        <h1>Olá Mundo</h1>
+      </div>
+  )
+}
+
+export default App
